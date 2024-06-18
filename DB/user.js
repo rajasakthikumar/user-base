aaeddc17fa857cb94ad253d71f60049359d00ac5
SAKTHI-PC\Sakthi <rajasakthikumarrajendran@gmail.com>
2024-06-18 14:38:45 +0530

    Adding zod and jwt

--- DB/user.js
+++ DB/user.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: String,
-    age: Number
+    age: Number,
+    email: String,
+    phone: String
 },{ collection: 'user'})
 
 const user = mongoose.model("User", userSchema)
